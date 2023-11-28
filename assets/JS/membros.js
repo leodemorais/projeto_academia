57ed6f82af96355344134f3611bd8c1ff2bbf408
fisiofaz <fisiofaz8@gmail.com>
2023-11-28 20:04:18 -0300

    feito: a pagina login membro e o css da mesma

---
+++ assets/JS/membros.js
@@ -0,0 +1,32 @@
+function addNewClass() {
+  const className = document.getElementById('class-name').value;
+  const classTime = document.getElementById('class-time').value;
+
+  if (className && classTime) {
+    const schedule = document.getElementById('schedule');
+    const newClass = document.createElement('div');
+    
+    // Obtendo a data atual para exibir junto com o horário da aula
+    const currentDate = new Date();
+    const formattedDate = currentDate.toLocaleDateString(); // Formata a data como string
+
+    newClass.innerHTML = `
+      <div class="schedule-scheduled">
+        <h2>${className}</h2>
+        <span><strong>Horário:</strong> ${classTime} - ${formattedDate}</span>
+      </div>
+      <div>
+        <button class="button" onclick="cancelClass(this)">Cancelar</button>
+      </div>`;
+    schedule.appendChild(newClass);
+  } else {
+    alert('Por favor, preencha todos os campos.');
+  }
+}
+
+function cancelClass(element) {
+  const classItem = element.parentNode.parentNode; // Para remover a div pai do botão
+  classItem.remove();
+}
+
+
