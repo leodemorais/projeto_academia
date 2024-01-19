e583914d42b5e2e2c8c3adbb69a5e404c0df8cba
fisiofaz <fisiofaz8@gmail.com>
2024-01-18 21:56:28 -0300

    feito: ajustes feito final do mercado e pagamento

--- assets/JS/pagamento.js
+++ assets/JS/pagamento.js
@@ -1,51 +1,57 @@
-document.getElementById('paymentForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-  var paymentMethod = document.querySelector('input[name="paymentMethod"]:checked').value;
-  switch (paymentMethod) {
-    case 'pix':
-      console.log('Informações para pagamento via Pix:');
-      var chavePix = document.getElementById('chavePix').value;
-      var valorPix = document.getElementById('valorPix').value;
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('paymentForm').addEventListener('submit', function (event) {
+    event.preventDefault();
 
-      // Lógica para pagamento via Pix
-      console.log('Informações para pagamento via Pix:');
-      console.log('Chave Pix:', chavePix);
-      console.log('Valor a pagar:', valorPix);
-      break;
-    case 'cartao':
-      console.log('Informações para pagamento com Cartão:');
-        var numeroCartao = document.getElementById('numeroCartao').value;
-        var nomeTitular = document.getElementById('nomeTitular').value;
-        var validadeCartao = document.getElementById('validadeCartao').value;
-        var codigoSeguranca = document.getElementById('codigoSeguranca').value;
-      
-        console.log('Informações para pagamento com Cartão:');
-        console.log('Número do Cartão:', numeroCartao);
-        console.log('Nome do Titular:', nomeTitular);
-        console.log('Validade:', validadeCartao);
-        console.log('Código de Segurança:', codigoSeguranca);
-        document.getElementById('numeroCartao').value = '';
-        document.getElementById('nomeTitular').value = '';
-        document.getElementById('validadeCartao').value = '';
-        document.getElementById('codigoSeguranca').value = '';      
-      break;
-    case 'boleto':
-      console.log('Informações para pagamento com Boleto:');
-      var cpfBoleto = document.getElementById('cpfBoleto').value;
-      var nomeBoleto = document.getElementById('nomeBoleto').value;
-      var enderecoBoleto = document.getElementById('enderecoBoleto').value;
-      var valorBoleto = document.getElementById('valorBoleto').value;
+    var paymentMethod = document.querySelector('input[name="paymentMethod"]:checked');
 
-      // Lógica para pagamento com Boleto
-      console.log('Informações para pagamento com Boleto:');
-      console.log('CPF:', cpfBoleto);
-      console.log('Nome Completo:', nomeBoleto);
-      console.log('Endereço:', enderecoBoleto);
-      console.log('Valor a pagar:', valorBoleto);
-      break;
-    default:
-      console.log('Selecione uma forma de pagamento');
-  }
+    if (paymentMethod) {
+      switch (paymentMethod.value) {
+        case 'pix':
+          console.log('Informações para pagamento via Pix:');
+          var chavePix = document.getElementById('chavePix').value;
+          var valorPix = document.getElementById('valorPix').value;
+
+          // Lógica para pagamento via Pix
+          console.log('Chave Pix:', chavePix);
+          console.log('Valor a pagar:', valorPix);
+          break;
+        case 'cartao':
+          console.log('Informações para pagamento com Cartão:');
+          var numeroCartao = document.getElementById('numeroCartao').value;
+          var nomeTitular = document.getElementById('nomeTitular').value;
+          var validadeCartao = document.getElementById('validadeCartao').value;
+          var codigoSeguranca = document.getElementById('codigoSeguranca').value;
+
+          // Lógica para pagamento com Cartão
+          console.log('Número do Cartão:', numeroCartao);
+          console.log('Nome do Titular:', nomeTitular);
+          console.log('Validade:', validadeCartao);
+          console.log('Código de Segurança:', codigoSeguranca);
+          document.getElementById('numeroCartao').value = '';
+          document.getElementById('nomeTitular').value = '';
+          document.getElementById('validadeCartao').value = '';
+          document.getElementById('codigoSeguranca').value = '';
+          break;
+        case 'boleto':
+          console.log('Informações para pagamento com Boleto:');
+          var cpfBoleto = document.getElementById('cpfBoleto').value;
+          var nomeBoleto = document.getElementById('nomeBoleto').value;
+          var enderecoBoleto = document.getElementById('enderecoBoleto').value;
+          var valorBoleto = document.getElementById('valorBoleto').value;
+
+          // Lógica para pagamento com Boleto
+          console.log('CPF:', cpfBoleto);
+          console.log('Nome Completo:', nomeBoleto);
+          console.log('Endereço:', enderecoBoleto);
+          console.log('Valor a pagar:', valorBoleto);
+          break;
+        default:
+          console.log('Selecione uma forma de pagamento');
+      }
+    } else {
+      console.log('Nenhum método de pagamento selecionado');
+    }
+  });
 });
 
 document.querySelectorAll('input[name="paymentMethod"]').forEach(function(radio) {
@@ -72,10 +78,23 @@ function exibirFormularioSelecionado() {
   document.querySelectorAll('.paymentDetails').forEach(details => {
     details.style.display = 'none';
   });
-
+  const total = localStorage.getItem("total")
   var selectedPayment = document.querySelector('input[name="paymentMethod"]:checked').value + 'Form';
-  document.getElementById(selectedPayment).style.display = 'flex';
+  document.getElementById(selectedPayment).style.display = 'flex'; 
+  // Adicionar código para exibir o valor no campo correspondente
+  switch (selectedPayment) {    
+    case 'pixForm':
+      document.getElementById('valorPix').value = total;
+      break;
+    case 'cartaoForm':
+      document.getElementById('valorCartao').value = total;
+      break;
+    case 'boletoForm':
+      document.getElementById('valorBoleto').value = total;
+      break;
+  }
 }
+
 
 // Adiciona um ouvinte de evento para cada botão de rádio
 document.querySelectorAll('input[name="paymentMethod"]').forEach(radio => {
@@ -88,3 +107,17 @@ document.querySelectorAll('input[name="paymentMethod"]').forEach(radio => {
 window.addEventListener('load', function () {
   exibirFormularioSelecionado();
 });
+
+// Função para carregar os dados do carrinho do localStorage
+function carregarCarrinhoDoLocalStorage() {
+  const itensCarrinhoArmazenados = JSON.parse(localStorage.getItem('itensCarrinho'));
+  const totalArmazenado = localStorage.getItem('total');
+
+  if (itensCarrinhoArmazenados && totalArmazenado) {
+    cartItems.length = 0;
+    Array.prototype.push.apply(cartItems, itensCarrinhoArmazenados);
+    total = parseFloat(totalArmazenado);
+    updateCart();
+  }
+}
+
